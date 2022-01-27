1a85d393da1fe7006703134064b9b88045e0a5ac
Tom Digby <m20tgd@hotmail.co.uk>
2022-01-27 11:01:15 +0000

    Add score, display and reset button. Change code so that it displays output in DOM rather than console and so that the game ends when one player reaches 5. Reset button allows game to be reset at the end or at any other time.

--- script.js
+++ script.js
@@ -5,21 +5,25 @@ let computerScore = 0;
 let playerSelection;
 let computerSelection;
 
-//Calculate who won and update console log with result
-//gameResult(playerScore, computerScore);
 
-//Add event listeners to buttons so a round is played when one is selected with the 
-//correct numerical input for the selection (0 for Rock, 1 for Paper and 2 for Scissors)
+//Assign DOM elements to variables
+const score = document.querySelector('.score');
+const display = document.querySelector('.display');
+const inputButtons = document.querySelectorAll('.inputButton');
 const rBtn = document.querySelector('#rockButton');
 const pBtn = document.querySelector('#paperButton');
 const sBtn = document.querySelector('#scissorsButton');
+const resetBtn = document.querySelector('.reset-button');
+//Add event listeners to input buttons so a round is played when one is selected with the 
+//correct numerical input for the selection (0 for Rock, 1 for Paper and 2 for Scissors)
 rBtn.addEventListener("click", () => playRound(0));
 pBtn.addEventListener("click", () => playRound(1));
 sBtn.addEventListener("click", () => playRound(2));
+//Add event listener to Reset button so the game is reset if it is pressed
+resetBtn.addEventListener("click", resetGame);
     
 //Create a function to play 5 rounds of Rock Paper Scissors
 function playRound(playerChoice){
-        //Get input from the player
         playerSelection = playerChoice;
         //Randomly select computer's choice
         computerSelection = computerChoice();
@@ -27,19 +31,9 @@ function playRound(playerChoice){
         let victory = establishWinner(playerSelection, computerSelection);
         //Update scores and announce round result
         roundResult(victory);
+        //If player or computer reach 5 points, then call endGame
+        if (playerScore >= 5 || computerScore >= 5) endGame();
 }
-
-// //Create a function that is called when a player makes their choice by clicking the 
-// //Rock, Paper or Scissors buttons. 
-// function playerChoice(choice){
-//     //Switch string input obtained from the player and return 0 for 'rock', 1 for 'paper' 
-//     //and 2 for 'scissors' to represent choice
-//     switch (choice){
-//         case "rock" : playRound(0);
-//         case "paper" : playRound(1);
-//         case "scissors" : playRound(2);
-//     }
-// }
 
 //Create a function to randomly make a selection for the computer
 function computerChoice(){
@@ -47,6 +41,8 @@ function computerChoice(){
     return num = Math.floor(Math.random()*3); //0 = Rock, 1 = Paper, 2 = Scissors
 }
 
+//Create a function to receive the player and computer choices and establish the outcome.
+//Returns True for player victory, False for computer victory and Null for draw
 function establishWinner(playerSelection, computerSelection){
 
     //Return null as a draw if the computer and player selections are the same
@@ -71,53 +67,29 @@ function establishWinner(playerSelection, computerSelection){
     }
 }
 
-//Create a function to get valid input from the player
-function getValidInput(){
-    //Get input from player and loop until valid choice provided
-    while (true){
-        input = prompt("Choose your weapon: \n Rock, Paper or Scissors?", "rock");
-        if (input != null){ //If player pressed cancel and input is null, then skip
-            //Change to all lower case for validation checks
-            input = input.toLowerCase();
-            //Check for valid entry and break out of loop if valid
-            if (input === "rock" || input === "paper" || input === "scissors"){
-                return input;
-            }
-        }
-        //If entry is not valid then alert message sent before the loop starts again
-        alert("Invalid choice. \n Please try again");
-    }
-}
-
 function roundResult(victory){
     //If victory is null, then announce a draw and don't alter the scores
     if (victory === null){
-            console.log("This round was a draw.");
+            display.innerText = "This round was a draw";
         }
         //Otherwise check to for victory, update score and announce result.
         else {
             if (victory){
                 playerScore++;
-                console.log(`You won this round! ${getSelectionString(playerSelection)} beats ${getSelectionString(computerSelection)}.`)
+                display.innerText = `You won this round! ${getSelectionString(playerSelection)} beats ${getSelectionString(computerSelection)}.`;
             }
             else {
                 computerScore++;
-                console.log(`You lost this round! ${getSelectionString(computerSelection)} beats ${getSelectionString(playerSelection)}.`)
+                display.innerText = `You lost this round! ${getSelectionString(computerSelection)} beats ${getSelectionString(playerSelection)}.`;
             }
+            //Update the score display with the new scores
+            updateScore();
         }
 }
 
-//Create function to announce overall winner of the game
-function gameResult(playerScore, computerScore){
-    if (playerScore > computerScore){ //Player victory
-        console.log(`Congratulations, you are the victor! \n You: ${playerScore} \n Computer: ${computerScore}`);
-    }
-    else if (playerScore < computerScore){ //Computer victory
-        console.log(`Commiserations, you were beaten by a machine! \n You: ${playerScore} \n Computer: ${computerScore}`);
-    }
-    else{ //Draw
-        console.log(`It was a draw! What a waste of time. \n You: ${playerScore} \n Computer: ${computerScore}`);
-    }
+//Create a function to update the score display
+function updateScore(){
+    score.innerText = `Player - ${playerScore} Computer - ${computerScore}`
 }
 
 //Create a function to return a string of the name of the selection from the number that represents it.
@@ -128,4 +100,25 @@ function getSelectionString(choice){
         case 2 : return "Scissors";
         default : console.log("Something went wrong with getSelectionString");
     }
+}
+
+//Create a function to end the game, display the results and disable buttons to 
+//prevent further rounds.
+function endGame(){
+    //Iterate through all the input buttons to disable them all
+    inputButtons.forEach(button => button.disabled = true);
+    //Display the final outcome
+    display.innerText = (playerScore > computerScore) ? "YOU WIN!" : "YOU LOSE!";
+}
+
+//Create a functions to reset the game. This is called by the Reset button
+function resetGame(){
+    //Reset the scores
+    playerScore = 0
+    computerScore = 0
+    updateScore();
+    //Reset the display text
+    display.innerText = "Please make your choice."
+    //Enable the input buttons
+    inputButtons.forEach(button => button.disabled = false);
 }
